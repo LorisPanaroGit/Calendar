5a26c18d9eefe6fca26a8f0e4a9c612bd2a36599
LorisPanaroGit <lorispanaro28@gmail.com>
2023-01-24 09:18:40 +0100

    First run

--- cmake-build-debug/CMakeFiles/3.21.1/CMakeCXXCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.21.1/CMakeCXXCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_CXX_COMPILER "C:/Clion_folder/CLion 2021.3.1/bin/mingw/bin/g++.exe")
+set(CMAKE_CXX_COMPILER "D:/Clion_folder/CLion 2021.3.2/bin/mingw/bin/g++.exe")
 set(CMAKE_CXX_COMPILER_ARG1 "")
 set(CMAKE_CXX_COMPILER_ID "GNU")
 set(CMAKE_CXX_COMPILER_VERSION "11.2.0")
@@ -21,11 +21,11 @@ set(CMAKE_CXX_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "C:/Clion_folder/CLion 2021.3.1/bin/mingw/bin/ar.exe")
-set(CMAKE_CXX_COMPILER_AR "C:/Clion_folder/CLion 2021.3.1/bin/mingw/bin/gcc-ar.exe")
-set(CMAKE_RANLIB "C:/Clion_folder/CLion 2021.3.1/bin/mingw/bin/ranlib.exe")
-set(CMAKE_CXX_COMPILER_RANLIB "C:/Clion_folder/CLion 2021.3.1/bin/mingw/bin/gcc-ranlib.exe")
-set(CMAKE_LINKER "C:/Clion_folder/CLion 2021.3.1/bin/mingw/bin/ld.exe")
+set(CMAKE_AR "D:/Clion_folder/CLion 2021.3.2/bin/mingw/bin/ar.exe")
+set(CMAKE_CXX_COMPILER_AR "D:/Clion_folder/CLion 2021.3.2/bin/mingw/bin/gcc-ar.exe")
+set(CMAKE_RANLIB "D:/Clion_folder/CLion 2021.3.2/bin/mingw/bin/ranlib.exe")
+set(CMAKE_CXX_COMPILER_RANLIB "D:/Clion_folder/CLion 2021.3.2/bin/mingw/bin/gcc-ranlib.exe")
+set(CMAKE_LINKER "D:/Clion_folder/CLion 2021.3.2/bin/mingw/bin/ld.exe")
 set(CMAKE_MT "")
 set(CMAKE_COMPILER_IS_GNUCXX 1)
 set(CMAKE_CXX_COMPILER_LOADED 1)
@@ -85,7 +85,7 @@ endif()
 
 
 
-set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES "C:/Clion_folder/CLion 2021.3.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include/c++;C:/Clion_folder/CLion 2021.3.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include/c++/x86_64-w64-mingw32;C:/Clion_folder/CLion 2021.3.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include/c++/backward;C:/Clion_folder/CLion 2021.3.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include;C:/Clion_folder/CLion 2021.3.1/bin/mingw/include;C:/Clion_folder/CLion 2021.3.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include-fixed;C:/Clion_folder/CLion 2021.3.1/bin/mingw/x86_64-w64-mingw32/include;/MinGW/include")
-set(CMAKE_CXX_IMPLICIT_LINK_LIBRARIES "stdc++;mingw32;gcc_s;gcc;moldname;mingwex;kernel32;pthread;advapi32;shell32;user32;kernel32;iconv;mingw32;gcc_s;gcc;moldname;mingwex;kernel32")
-set(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "C:/Clion_folder/CLion 2021.3.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0;C:/Clion_folder/CLion 2021.3.1/bin/mingw/lib/gcc;C:/Clion_folder/CLion 2021.3.1/bin/mingw/x86_64-w64-mingw32/lib;C:/Clion_folder/CLion 2021.3.1/bin/mingw/lib;/MinGW/lib")
+set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES "D:/Clion_folder/CLion 2021.3.2/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include/c++;D:/Clion_folder/CLion 2021.3.2/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include/c++/x86_64-w64-mingw32;D:/Clion_folder/CLion 2021.3.2/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include/c++/backward;D:/Clion_folder/CLion 2021.3.2/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include;D:/Clion_folder/CLion 2021.3.2/bin/mingw/include;D:/Clion_folder/CLion 2021.3.2/bin/mingw/lib/gcc/x86_64-w64-mingw32/11.2.0/include-fixed;D:/Clion_folder/CLion 2021.3.2/bin/mingw/x86_64-w64-mingw32/include")
+set(CMAKE_CXX_IMPLICIT_LINK_LIBRARIES "")
+set(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "")
 set(CMAKE_CXX_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Clion_folder/Projetcs/Calendar
+# Install script for directory: D:/Clion_folder/Projects/Calendar
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Clion_folder/CLion 2021.3.1/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "D:/Clion_folder/CLion 2021.3.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Clion_folder/Projetcs/Calendar/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Clion_folder/Projects/Calendar/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
