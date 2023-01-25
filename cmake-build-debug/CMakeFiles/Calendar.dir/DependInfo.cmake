7b8c244f19fa6e4f37d38a3a7f29dea4c7cf1fd9
LorisPanaroGit <lorispanaro28@gmail.com>
2023-01-25 01:40:10 +0100

    dummy commit

---
+++ cmake-build-debug/CMakeFiles/Calendar.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Clion_folder/Projects/Calendar/CHAR.cpp" "CMakeFiles/Calendar.dir/CHAR.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/CHAR.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/COLUMN.cpp" "CMakeFiles/Calendar.dir/COLUMN.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/COLUMN.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/COLUMNBASE.cpp" "CMakeFiles/Calendar.dir/COLUMNBASE.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/COLUMNBASE.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/COLUMNBOOL.cpp" "CMakeFiles/Calendar.dir/COLUMNBOOL.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/COLUMNBOOL.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/COLUMNCHAR.cpp" "CMakeFiles/Calendar.dir/COLUMNCHAR.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/COLUMNCHAR.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/COLUMNDATE.cpp" "CMakeFiles/Calendar.dir/COLUMNDATE.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/COLUMNDATE.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/COLUMNFLOAT.cpp" "CMakeFiles/Calendar.dir/COLUMNFLOAT.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/COLUMNFLOAT.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/COLUMNINT.cpp" "CMakeFiles/Calendar.dir/COLUMNINT.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/COLUMNINT.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/COLUMNTEXT.cpp" "CMakeFiles/Calendar.dir/COLUMNTEXT.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/COLUMNTEXT.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/COLUMNTIME.cpp" "CMakeFiles/Calendar.dir/COLUMNTIME.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/COLUMNTIME.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/DATABASE.cpp" "CMakeFiles/Calendar.dir/DATABASE.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/DATABASE.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/DATE.cpp" "CMakeFiles/Calendar.dir/DATE.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/DATE.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/FLOAT.cpp" "CMakeFiles/Calendar.dir/FLOAT.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/FLOAT.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/INT.cpp" "CMakeFiles/Calendar.dir/INT.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/INT.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/TABLE.cpp" "CMakeFiles/Calendar.dir/TABLE.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/TABLE.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/TIME.cpp" "CMakeFiles/Calendar.dir/TIME.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/TIME.cpp.obj.d"
+  "D:/Clion_folder/Projects/Calendar/main.cpp" "CMakeFiles/Calendar.dir/main.cpp.obj" "gcc" "CMakeFiles/Calendar.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
